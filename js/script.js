51a12b27146a5599693f3af12a7d8a46ea3d3775
Artem <hitrick84@ya.ru>
2015-07-23 19:05:50 +0400

    add baloon on click with all markers

--- js/script.js
+++ js/script.js
@@ -7,64 +7,60 @@
 	var coords = [
 		{
 			data: [55.76, 37.44],
-		   content: "<div><a href=\"#\">Placemark 1</a></div>"
+		   content: "Content 1"
 		},
 		{
 			data: [55.16, 38.54],
-			content: "Content -- "
+			content: "Content 2"
 		},
 		{
 			data: [55.46, 36.64],
-			content: "Content -- "
+			content: "Content 3"
 		},
 		{
 			data: [55.71, 37.61],
-			content: "Content -- "
+			content: "Content 4"
 		},
 		{
 			data: [56.19, 38.59],
-			content: "Content -- "
+			content: "Content 5"
 		},
 		{
 			data: [54.29, 36.62],
-			content: "Content -- "
+			content: "Content 6"
 		}
 	];
+	function getContent(arr, str) {
+		var content = "<div class=\"baloon_content\">";
+		arr.forEach(function(item) {
+			if(item.content !== str) {
+				content += "<p data-coords=" + item.data.join('-') + ">" + item.content + "</p>";
+			}
+		});
+		content += "</div>";
+		return content;
+	}
 	function createMark(item) {
 		var rand = Math.floor(Math.random()*100);
 		var myPlacemark = new ymaps.Placemark(item.data, {
-			//hintContent: 'Content_random ' + rand,
-			//balloonContent: item.content + rand
+			hintContent: item.content
 		});
 		myMap.geoObjects.add(myPlacemark);
 		myPlacemark.events.add('click', function(e) {
 				var coords = e.get("coords");
-				showHint(e, coords);
+				showHint(e, myPlacemark.properties._6c.hintContent);
 				myMap.panTo(coords, {duration: 1000});
 			});
 	}
-
-	//function getClickCoords(e) {
-	//	myMap.events.add("click", function(e) {
-	//		var coords = e.get("coords");
-	//		myMap.panTo(coords, {duration: 1000});
-	//	});
-	//}
-	function showHint(e, coords) {
+	function showHint(e, str) {
 			myMap.balloon.close();
-			var coords = e.get('coords');
-			myMap.balloon.open(coords, {
-				contentHeader:'Событие!',
-				contentBody:'<p>Кто-то щелкнул по карте.</p>' +
-				'<p>Координаты щелчка: ' + [
-					coords[0].toPrecision(2),
-					coords[1].toPrecision(2)
-				].join(', ') + '</p>',
+			var crds = e.get('coords');
+			myMap.balloon.open(crds, {
+				contentHeader:'Метки!',
+				contentBody:getContent(coords, str),
 				contentFooter:'<sup>Made by Hitrick</sup>'
 			});
 		}
-
-
 	function init() {
 		myMap = new ymaps.Map("map", {
 			center: [55.76, 37.64],
@@ -72,5 +68,4 @@
 		});
 		coords.forEach(createMark);
 	}
-
 })(window);
